5a8e4a3e127addbd95db1b910e482c437135e5ad
Daksh Choudhary <ch.daksh01@gmail.com>
2024-11-05 19:12:15 +0530

    added scroll effect on the screen of the laptop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Environment, ScrollControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import MacContainer from './Components/MacContainer'
@@ -7,11 +7,13 @@ import "./stylesheets/style.css"
 const App = () => {
   return (
     <Canvas camera={{fov:12,position:[0,-10,220]}}>
-      <OrbitControls />
       <Environment files={[
         "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/studio_small_09_2k.hdr",
       ]} />
-      <MacContainer />  
+
+      <ScrollControls>
+        <MacContainer />  
+      </ScrollControls>
     </Canvas>
   )
 }
